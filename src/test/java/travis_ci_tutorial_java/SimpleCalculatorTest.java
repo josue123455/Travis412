ad9639ad45966255ac206291dd44b5f9ccd3fe41
josue123455 <63947727+josue123455@users.noreply.github.com>
2023-02-27 01:53:44 -0500

    Update SimpleCalculatorTest.java

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,19 +11,34 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 2);
 	}
-	
+	@Test
+	public void testAdd2plus2() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(2, 2), 4);
+	}
 	
 	@Test
 	public void testminus() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
 	}
+	@Test
+	public void testminus5minus1() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.minus(5, 1), 4);
+	}
 		
+	
 
 	@Test
 	public void testmultiply() {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.multiply(5, 3), 15);
+	}
+	@Test
+	public void testmultiply5mulitply2() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(5, 2), 10);
 	}
 		
 	@Test
@@ -32,7 +47,11 @@ public class SimpleCalculatorTest {
 		assertEquals(calc.divide(4, 2), 2);
 	}
 	
+	@Test
+	public void testdividebyZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(4, 0), 0);
+	}
 	
 // last curly bracket for code 	
 }
-
